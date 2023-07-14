87cac7e802ffec57f93a4787b54115fd673ed397
youngeun <jessicak0828@gmail.com>
2023-07-14 22:58:07 +0900

    feat: 공통 컴포넌트 Header 생성

---
+++ src/components/common/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.styles.scss';
+import NavBar from '@/components/common/NavBar';
+
+export default function Header() {
+	return (
+		<header>
+			<h1>Frontend Developer</h1>
+			<NavBar />
+		</header>
+	);
+}
