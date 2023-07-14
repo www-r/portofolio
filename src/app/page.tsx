fc95f9c1ed7080a9cd12f0c376208b4237283214
youngeun <jessicak0828@gmail.com>
2023-07-14 23:00:08 +0900

    feat: MainPage 에 만든 컴포넌트들 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Header from '@/components/common/Header';
+import SectionSelfIntro from '@/components/SectionSelfIntro';
 export default function Home() {
-	return <></>;
+	return (
+		<>
+			<Header />
+			<SectionSelfIntro />
+			
+		</>
+	);
 }
