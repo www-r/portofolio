4b8ad71974a7884e156367baa2cdade52ccec06c
youngeun <jessicak0828@gmail.com>
2023-07-14 22:57:14 +0900

    feat: 공통 컴포넌트 Button 생성

---
+++ src/components/common/Button/NavigateButton.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './NavigateButton.styles.scss';
+export default function NavigateButton({ children }) {
+	return <button className="navigate-btn">{children}</button>;
+}
