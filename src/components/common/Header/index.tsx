d7ecd5faf3231146e6122ac719178a28ecce4c2b
youngeun <jessicak0828@gmail.com>
2023-07-20 22:15:57 +0900

    feat: scroll 에 따른 Header 에서의 NavBar 유무

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,25 +1,40 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Link from 'next/link';
 import './Header.styles.scss';
 import NavBar from '@/components/common/NavBar';
-
 export default function Header() {
 	const [hidden, setHidden] = useState(false);
+	let prevScrollY = 0;
+	onscroll = () => {
+		//scroll 방향에 따른 navBar 유무
 
-	useEffect(() => {
-		window.addEventListener('scroll', () => {
-			let h1 = document.getElementsByClassName('header-title');
-			if (scrollY === 94) setHidden(!hidden);
-			if (scrollY > 94) {
-				h1[0].className = 'header-title shrinked';
-			} else {
-				h1[0].className = 'header-title';
-			}
-		});
-	}, []);
+		let currentScrollY = scrollY;
+		if (prevScrollY < currentScrollY) {
+			setHidden(true);
+			prevScrollY = scrollY;
+			return;
+		} else if (prevScrollY > currentScrollY) {
+			setHidden(false);
+			prevScrollY = scrollY;
+		}
+	};
+	// useEffect(() => {
+	// 	window.addEventListener('scroll', () => {
+	// 		let h1 = document.getElementsByClassName('header-title');
+	// 		if (scrollY === 94) setHidden(!hidden);
+	// 		if (scrollY > 94) {
+	// 			h1[0].className = 'header-title shrinked';
+	// 		} else {
+	// 			h1[0].className = 'header-title';
+	// 		}
+	// 	});
+	// }, []);
 	return (
 		<header>
-			<h1 className="header-title">Frontend Developer</h1>
+			<Link href="/">
+				<h1 className="header-title">Frontend Developer</h1>
+			</Link>
 			{!hidden && <NavBar />}
 		</header>
 	);
