313be42031c20700812fd8fe6f992ccfbc75a581
youngeun <jessicak0828@gmail.com>
2023-07-20 21:39:40 +0900

    assets & feat : 필요한 이미지 추가 및 한 파일에서 export 시키기

---
+++ public/images.js
@@ -0,0 +1,5 @@
+export * as frame from './images/frame.svg';
+export * as ocean from './images/ocean.svg';
+export * as firstPhoto from './images/first.png';
+export * as secondPhoto from './images/second.png';
+export * as thirdPhoto from './images/third.png';
