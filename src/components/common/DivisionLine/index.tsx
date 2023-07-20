2e7dd6709528126a9fffe846b5268ff399646055
youngeun <jessicak0828@gmail.com>
2023-07-20 18:26:04 +0900

    feat: 공통 컴포넌트 DivisionLine

--- src/components/common/DivisionLine/index.tsx
+++ src/components/common/DivisionLine/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import './DivisionLine.styles.scss'
-interface DivisionLineProps {
-	direction: 'row' | 'column';
-}
-
+import './DivisionLine.styles.scss';
+import { DivisionLineProps } from '@/interfaces/ComponentProps';
 export default function DivisionLine({ direction }: DivisionLineProps) {
 	return <>{direction === 'row' ? <div className="row"></div> : <div className="column"></div>}</>;
 }
