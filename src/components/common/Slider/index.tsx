79c96e43b39ede455e5dba89dff839bdebcecf1a
youngeun <jessicak0828@gmail.com>
2023-07-20 18:31:17 +0900

    feat: 공통 컴포넌트 Slider (~ing)

--- src/components/common/Slider/index.tsx
+++ src/components/common/Slider/index.tsx
@@ -1,30 +1,21 @@
 'use client';
 import React from 'react';
-import Swiper from 'swiper';
-import 'swiper/scss/scrollbar';
+import Image from 'next/image';
+import './Slider.styles.scss';
+import { SliderProps } from '@/interfaces/ComponentProps';
 
-export default function Slider() {
-	const swiper = new Swiper('.swiper', {
-		// Optional parameters
-		speed: 400,
-		spaceBetween: 100,
-		direction: 'vertical',
-		loop: false,
-
-		// And if we need scrollbar
-		scrollbar: {
-			el: '.swiper-scrollbar',
-		},
-	});
+export default function Slider({ width = '120rem', array }: SliderProps) {
+	console.log(array);
+	const sliderClickHandler = () => {
+		console.log('dd');
+	};
 	return (
-		<div>
-			<div className="swiper">
-				<div className="swiper-wrapper">
-					<div className="swiper-slide">Slide 1</div>
-					<div className="swiper-slide">Slide 2</div>
-					<div className="swiper-slide">Slide 3</div>
-				</div>
-				<div className="swiper-scrollbar"></div>
+		<div className="slider-wrapper">
+			<div className="slider" onClick={sliderClickHandler} style={{ width: `${width}` }}>
+				{array.length !== 0 &&
+					array.map((slide, index) => (
+						<Image key={index} src={slide.default.src} alt="slide" width={400} height={500} />
+					))}
 			</div>
 		</div>
 	);
