0fdf02249bf7abe3633ca8be311c302bfe2e9a63
youngeun <jessicak0828@gmail.com>
2023-07-20 22:16:30 +0900

    delete

--- public/image/images.js
+++
@@ -1,5 +0,0 @@
-export * as frame from './frame.svg';
-export * as ocean from './ocean.svg';
-export * as firstPhoto from './first.png';
-export * as secondPhoto from './second.png';
-export * as thirdPhoto from './third.png';
