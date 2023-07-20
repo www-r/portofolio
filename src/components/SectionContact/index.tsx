c0cb1038d2e1bac51a16f12acd238cd0b8b9f4b3
youngeun <jessicak0828@gmail.com>
2023-07-20 18:33:47 +0900

    feat: 컴포넌트 Section Contact (~ing)

---
+++ src/components/SectionContact/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SectionContact.styles.scss';
+import Section from '@/components/common/Section';
+export default function SectionContact() {
+	return (
+		<Section className="contact-section">
+			<h2>Contact</h2>
+			<h3>이쪽으로 연락주시면 감사하겠습니다.</h3>
+		</Section>
+	);
+}
