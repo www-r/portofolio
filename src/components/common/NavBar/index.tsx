e2fec90680beaa02d84e1faa5600f68bbd726bd0
youngeun <jessicak0828@gmail.com>
2023-07-20 18:30:03 +0900

    feat: 공통 컴포넌트 NavBar

--- src/components/common/NavBar/index.tsx
+++ src/components/common/NavBar/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import './NavBar.styles.scss';
 export default function index() {
+	const navLinkHandler = (item) => {
+		switch (item) {
+			case 'About':
+				scrollTo(0, 420);
+				break;
+			case 'Skills':
+				scrollTo(0, 1625);
+				break;
+			case 'Projects':
+				scrollTo(0, 2350);
+				break;
+			case 'Contact':
+				break;
+		}
+	};
 	return (
 		<nav>
-			<div className='navbar-item'>자기소개</div>
-			<div className='navbar-item'>기술스텍</div>
-			<div className='navbar-item'>포트폴리오</div>
-			<div className='navbar-item'>Contact</div>
+			<div className="navbar-item" onClick={() => navLinkHandler('About')}>
+				About
+			</div>
+			<div className="navbar-item" onClick={() => navLinkHandler('Skills')}>
+				Skills
+			</div>
+			<div className="navbar-item" onClick={() => navLinkHandler('Projects')}>
+				Projects
+			</div>
+			<div className="navbar-item" onClick={() => navLinkHandler('Contact')}>
+				Contact
+			</div>
 		</nav>
 	);
 }
