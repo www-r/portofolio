3a14913eb7513546b14b31762223411c664a4a73
youngeun <jessicak0828@gmail.com>
2023-07-20 18:19:54 +0900

    assets:

---
+++ public/image/images.js
@@ -0,0 +1,5 @@
+export * as frame from './frame.svg';
+export * as ocean from './ocean.svg';
+export * as firstPhoto from './first.png';
+export * as secondPhoto from './second.png';
+export * as thirdPhoto from './third.png';
