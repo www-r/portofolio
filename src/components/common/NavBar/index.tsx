15f1fc274c626f25bfccbc33ee07c906f947426b
youngeun <jessicak0828@gmail.com>
2023-07-20 22:00:54 +0900

    feat: 항목에 Archive 추가

--- src/components/common/NavBar/index.tsx
+++ src/components/common/NavBar/index.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import './NavBar.styles.scss';
 export default function index() {
-	const navLinkHandler = (item) => {
+	const navLinkHandler = (item: string) => {
 		switch (item) {
 			case 'About':
-				scrollTo(0, 420);
+				scrollTo(0, 1280);
+				break;
+			case 'Archive':
+				scrollTo(0, 1950);
 				break;
 			case 'Skills':
-				scrollTo(0, 1625);
+				scrollTo(0, 2900);
 				break;
 			case 'Projects':
-				scrollTo(0, 2350);
+				scrollTo(0, 3835);
 				break;
 			case 'Contact':
+				scrollTo(0, 5000);
 				break;
 		}
 	};
+
 	return (
 		<nav>
 			<div className="navbar-item" onClick={() => navLinkHandler('About')}>
 				About
+			</div>
+			<div className="navbar-item" onClick={() => navLinkHandler('Archive')}>
+				Archive
 			</div>
 			<div className="navbar-item" onClick={() => navLinkHandler('Skills')}>
 				Skills
