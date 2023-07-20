4f21e798c17b5337d7b9748e81228b725e47cc85
youngeun <jessicak0828@gmail.com>
2023-07-20 18:28:55 +0900

    feat: 공통 컴포넌트 Header

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import './Header.styles.scss';
 import NavBar from '@/components/common/NavBar';
 
 export default function Header() {
+	const [hidden, setHidden] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			let h1 = document.getElementsByClassName('header-title');
+			if (scrollY === 94) setHidden(!hidden);
+			if (scrollY > 94) {
+				h1[0].className = 'header-title shrinked';
+			} else {
+				h1[0].className = 'header-title';
+			}
+		});
+	}, []);
 	return (
 		<header>
-			<h1>Frontend Developer</h1>
-			<NavBar />
+			<h1 className="header-title">Frontend Developer</h1>
+			{!hidden && <NavBar />}
 		</header>
 	);
 }
