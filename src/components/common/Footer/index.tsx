c848269933660381297742fb9684e0928a50c7de
youngeun <jessicak0828@gmail.com>
2023-07-20 21:48:44 +0900

    feat: 공통 컴포넌트 Footer

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Button from '../Button/Button';
 import './Footer.styles.scss';
+import { githubMarkLogoWhite, notionIcon, instagramIcon, threadsIcon } from '../../../../public/icons';
 export default function Footer() {
-	return <footer>index</footer>;
+	return (
+		<footer>
+			<div>
+				<ul className="contact-buttons-container">
+					<li>
+						<Link href="https://github.com/www-r">
+							<Button>
+								<Image src={githubMarkLogoWhite} alt="github icon" height={48} />
+							</Button>
+						</Link>
+					</li>
+					<li>
+						<Link href="#">
+							<Button>
+								<Image src={notionIcon} alt="notion icon" height={48} />
+							</Button>
+						</Link>
+					</li>
+					<li>
+						<Link href="https://instagram.com/wwwr_kye?igshid=OGQ5ZDc2ODk2ZA==">
+							<Button>
+								<Image src={instagramIcon} alt="instagram icon" height={48} />
+							</Button>
+						</Link>
+					</li>
+					<li>
+						<Link href={'#'}>
+							<Button>
+								<Image src={threadsIcon} alt="threads icon" height={48} />
+							</Button>
+						</Link>
+					</li>
+				</ul>
+				<p>© 2023. Kim Young En. All rights reserved.</p>
+			</div>
+		</footer>
+	);
 }
