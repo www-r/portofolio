0986d7fea16bace542ea7c04f4bb44a278d11df0
youngeun <jessicak0828@gmail.com>
2023-07-20 18:39:33 +0900

    feat: App Page(Main Page) & global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
+import Image from 'next/image';
 import Header from '@/components/common/Header';
-import SectionSelfIntro from '@/components/SectionSelfIntro';
+import SectionAbout from '@/components/SectionAbout';
+import SectionArchive from '@/components/SectionArchive';
+import SectionSkills from '@/components/SectionSkills';
+import SectionProjects from '@/components/SectionProjects';
+import SectionContact from '@/components/SectionContact';
+import Footer from '@/components/common/Footer';
+import Button from '@/components/common/Button/Button';
+import { arrowTopIcon } from '../../public/icons/icons';
+
 export default function Home() {
 	return (
 		<>
 			<Header />
-			<SectionSelfIntro />
-			
+			<div style={{ height: '19rem', width: '100vw' }}></div>
+			<SectionAbout />
+			<SectionArchive />
+			<SectionSkills />
+			<SectionProjects />
+			<SectionContact />
+			<Footer />
+			<Button className="scroll-top-btn">
+				<Image src={arrowTopIcon} alt="scroll top button" width={48} />
+			</Button>
 		</>
 	);
 }
